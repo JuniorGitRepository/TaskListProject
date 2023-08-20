94fd8af2203ce2270a69ece631be135317b81b9b
Junior <127817231+JuniorGitRepository@users.noreply.github.com>
2023-08-20 03:19:55 +0000

    Update 3 files

--- src/App.js
+++ src/App.js
@@ -1,14 +1,41 @@
+import React,{useState} from "react";
 import "./styles.css";
 import Navbar from "./componentes/NavBar/Navbar";
 import TaskList from "./componentes/TaskList/TaskList";
+
+const task = {
+  id:0,
+  title:'Nova Tarefa',
+  state:'pendente'
+}
+
+let idAcc = 0;
+const generateId = () =>{
+  idAcc = idAcc + 1;
+  return idAcc;
+}
+
 export default function App() {
+  const [tasks, setTasks] = useState([]);
+
+  const addTask = (title, state) =>{
+    console.log("função sendo chamada em App")
+    const newTask = {
+      id:generateId(),
+      title,
+      state
+    };
+
+    setTasks((listTasks)=>{
+        return[...listTasks,newTask];
+    });
+  }
   return (
     <div className="App">
       <Navbar />
       <div className="container">
-        <TaskList title="Pendente" />
-        <TaskList title="Fazendo" />
-        <TaskList title="Completa" />
+        <TaskList title="Pendente" onAddTask={addTask} />
+        
       </div>
     </div>
   );
--- src/componentes/TaskList/TaskList.js
+++ src/componentes/TaskList/TaskList.js
@@ -1,27 +1,23 @@
-import React, {useState} from "react";
+import React from "react";
 import "./TaskList.css";
 import PropTypes from "prop-types";
 
-export default function TaskList({ title }) {
-  const [count, setCount] = useState(0)
-
-  const increment = () =>{
-    setCount((currentCount)=>{
-      return currentCount + 1;
-    });
-  }
+export default function TaskList({ title, onAddTask}) {
+  const addTask = () => {
+    console.log("chamada dentro do tasklist")
+    onAddTask("Nova Tarefa","Pendente");
+  };
 
   return (
     <div className="tasklist">
       <span className="title">{title}</span>
-      <div className="content">{count}
-        <button onClick={increment}>
-          incrementar
-        </button>  
+      <div className="content">
+        <button onClick={addTask}>Adicionar Tarefa</button>
       </div>
     </div>
   );
 }
 TaskList.propTypes = {
-  title: PropTypes.string.isRequired
+  title: PropTypes.string.isRequired,
+  onAddTask: PropTypes.func.isRequired
 };
